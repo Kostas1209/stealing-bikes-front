74915f8e0a9700e832d07f712001cf7e4fde19c6
Kostya Kostyanitsa <kostyanitsakonstantyn@gmail.com>
2020-07-01 14:07:57 +0300

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <footer>Kostya Kostyanitsa 2020</footer>
   </div>
 </template>
 
