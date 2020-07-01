9ac7effdce3c153fdfe66b94f889930314512318
Kostya Kostyanitsa <kostyanitsakonstantyn@gmail.com>
2020-07-01 19:04:49 +0300

    create basic user route

--- src/components/UserLostBikes.vue
+++ src/components/UserLostBikes.vue
@@ -1,14 +1,41 @@
 <template>
     <div>
         user lost bikes
+        <h1 v-show="!isVisible">...loading</h1>
+        <ul>
+            <li v-show="isVisible" v-for="bike in lostBikes">
+                <p v-for="(value,key) in bike">{{key}} : {{value}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script lang="ts">
-    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import config from "../../config";
 
-    @Component
-    export default class UserLostBikes extends Vue {
-        
+    export default {
+        name: "UserLostBikes",
+        data()
+        {
+            return {
+                isVisible: false,
+                lostBikes: []
+            }
+        },
+        methods: {
+            async getData()
+            {
+                let res = await fetch(`${config.API_URL}/api/user/report?userId=1`)
+                console.log(res)
+                let data = await res.json();
+                console.log(data)
+                this.lostBikes = data.data;
+                this.isVisible = true
+            }
+        },
+        beforeMount()
+        {
+            this.getData()
+        }
     }
 </script>
